2303e9115e1fa97bde2d258889a6c060a1eeb279
tamako8782 <study.work.yama@gmail.com>
2024-09-28 10:12:24 +0900

    MS_create react api fetch code for list brows

--- frontend/myapp/src/App.jsx
+++ frontend/myapp/src/App.jsx
@@ -8,8 +8,13 @@ export const App = ()=> {
   const [showCreateModal,setShowCreateModal] = useState(false);
   const [showDetailModal,setShowDetailModal] = useState(false);
   const [inCompTask,setInCompTask] = useState([]);
+  const [finishTask,setFinishTask] = useState([]);
   const apipath_env = process.env.REACT_APP_BACKEND_PATH;
+  const [error, setError] = useState(null);
 
+  const listpath  = apipath_env + "/task";
+
+  //モーダル参照用のコード
   const ShowCreateModal =()=> {
     setShowCreateModal(true);
   };
@@ -18,21 +23,29 @@ export const App = ()=> {
     setShowDetailModal(true);
   };
 
+  //リストをAPIから取得するためのコード
   useEffect( ()=>{
-    fetch(apipath_env)
+    fetch(listpath)
       .then(response => {
-        if (response.ok)
-
+        if (!response.ok){
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
       })
-      .then(data ={
-
+      .then(data =>{
+        const inpro = data.filter(task => task.state === "Inprogress");
+        const fin = data.filter(task => task.state === "Finished");
+        setInCompTask(inpro);
+        setFinishTask(fin);
       })
-      .catch(error =>{
-
+      .catch(error => {
+        setError(error); // エラーがあればエラーメッセージをセット
       });
   },[]);  
 
+  
 
+  //以下HTML表示
   return (
     <>
     <header>
@@ -52,33 +65,41 @@ export const App = ()=> {
       <p>incompletetask</p>
       <ul>
         {/*ここにタスクリストが一覧表示*/}
-        <li>
+        {inCompTask.map(task => (
+          <li key={task.id}>
           <p>
-            test
-            </p>
+            {task.title}
+          </p>
             <button className="comp-button">complete</button>
             <button onClick={ShowDetailModal} className="detail-button">detail</button>
             <DetailModal showFlag={showDetailModal} setShowDetailModal={setShowDetailModal}/>
 
 
-        </li>
+          </li>
+
+        ))}
       </ul>
     </div>
     <div className='finishedtask'>
       <p>finishedtask</p>
       <ul>
         {/*ここにタスクリストが一覧表示*/}
-        <li>
-        <p>test</p>
+        {finishTask.map(task => (
+        <li key={task.id}>
+        <p>
+          {task.title}
+          </p>
           <button className="restore-button">restore</button>
           <button onClick={ShowDetailModal} className="detail-button">detail</button>
           <DetailModal  showFlag={showDetailModal} setShowDetailModal={setShowDetailModal}/>
 
 
         </li>
+      ))}
       </ul>
 
     </div>
+
 
     </main>
     <footer>
