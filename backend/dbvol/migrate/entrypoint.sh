a312af588364bf996e250419fe4b88c0058b2979
tamako8782 <study.work.yama@gmail.com>
2024-09-30 07:43:09 +0900

    migrate on kubernetes is numa

--- backend/dbvol/migrate/entrypoint.sh
+++ backend/dbvol/migrate/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-/wait
+if [ -f /wait ]; then
+  /wait  # docker-compose-waitを実行して、MySQLが起動するのを待つ
+fi
+
 /migrate \
   -path $MIGRATIONS_DIR \
   -database "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:3306)/${MYSQL_DATABASE}" \
