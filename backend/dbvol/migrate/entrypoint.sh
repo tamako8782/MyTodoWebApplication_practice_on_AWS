4ad301bf08eda001e327024a57c9895156e68963
tamako8782 <study.work.yama@gmail.com>
2024-10-03 07:13:58 +0900

    migrationを実施するすべを考える

--- backend/dbvol/migrate/entrypoint.sh
+++ backend/dbvol/migrate/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-if [ -f /wait ]; then
-  /wait  # docker-compose-waitを実行して、MySQLが起動するのを待つ
-fi
 
 /migrate \
-  -path $MIGRATIONS_DIR \
-  -database "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:3306)/${MYSQL_DATABASE}" \
+  -database \"mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:3306)/${MYSQL_DATABASE}\" \
   $@
