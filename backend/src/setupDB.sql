5fe8584d00f690de66abd77246570e69fc70c74a
tamako8782 <study.work.yama@gmail.com>
2024-10-15 14:26:53 +0900

    nazonomigrationgaolddattawokaisyou

--- backend/src/setupDB.sql
+++ backend/src/setupDB.sql
@@ -1,12 +1,12 @@
-create table if not exists task ( 
-task_id integer unsigned auto_increment primary key, 
-title varchar(100) not null,
-contents text not null,
-task_state text not null,
-updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+-- マイグレーションのアップ用（適用）
+create table if not exists task (
+    task_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    contents TEXT,
+    task_state ENUM('InComplete', 'Finished','NotDoTask') DEFAULT 'NotDoTask',
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
 
 insert into task (title, contents,task_state, created_at) values 
 
